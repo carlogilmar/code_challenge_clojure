ea8761f70f1bc8e4a9305e6b7089099da082d9ed
Carlo Gilmar <carlogilmar12@gmail.com>
2019-12-29 00:08:01 -0600

    Refactor verify_account_active

--- src/code_challenge/core.clj
+++ src/code_challenge/core.clj
@@ -11,15 +11,20 @@
 
 (defn verify_account_active
   ([previous_validation]
-    (cond
-      (= (first previous_validation) :error)
-        [:error (first (rest previous_validation))]
-      (and (= (nth previous_validation 0) :ok)
-           (= (get (nth previous_validation 1) :active_card) true))
-        [:ok (nth previous_validation 1) (nth previous_validation 2)]
-      (and (= (nth previous_validation 0) :ok)
-           (= (get (nth previous_validation 1) :active_card) false))
-        [:error :card_not_active]
-      )
+
+   (def response (nth previous_validation 0))
+
+   (cond
+     (= response :error)
+     [:error (first (rest previous_validation))]
+
+     (and (= response :ok)
+          (= (get (nth previous_validation 1) :active_card) false))
+     [:error :card_not_active]
+
+     (and (= (nth previous_validation 0) :ok)
+          (= (get (nth previous_validation 1) :active_card) true))
+     [:ok (nth previous_validation 1) (nth previous_validation 2)]
+     )
    )
   )
